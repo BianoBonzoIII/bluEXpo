d4bfffc5015d518f7c593ea6140ccd710d2b6a04
Brian Lin <blin5@stuy.edu>
2018-05-17 18:19:53 -0400

    Update test cases, methods, inst vars, and comments

--- rmed/RunMed.java
+++ rmed/RunMed.java
@@ -2,6 +2,17 @@
  * class RunMed
  * Implements a min heap and max using an ArrayList as underlying container
    to find the median of a stream of numbers
+
+Algo: 
+1.) Add the upper half of the stream of Integers to the maxHeap and add the
+    lower half of the stream of Integers to the minHeap. If the next value is
+    < root of maxHeap, add the value to maxHeap. If not, add to minHeap.  
+
+2.) Remove from maxHeap and add those removed values to minHeap until the 
+    heaps are balanced ( size difference < 2 )
+
+3.) If the heap sizes are = --> return the median of the roots of both heaps 
+    If the heap sizes are != --> return the root of maxHeap 
 *****************************************************/
 
 import java.util.ArrayList;
@@ -21,10 +32,49 @@ public class RunMed
 	maxHeap = new ALHeapMax();
     }
 
+    /*****************************************************
+     * getMedian()  ---  returns the median from a stream of ints
+     * Postcondition: Stream of ints remain unchanged
+     *****************************************************/
+    public Integer getMedian() {
+    }
+    
+    /*****************************************************
+     * add(Integer) 
+     * Inserts an element in the appropiate heap
+     * Postcondition: Tree exhibits appropiate heap property.
+     *****************************************************/
+    public void add( Integer newVal ) {
+    }
+    
     //main method for testing
     public static void main( String[] args )
     {
+	RunMed beck = new RunMed();
+	
+	for (int i = 0; i < 5; i++) { // 5 total elements
+	    Integer call = new Integer( (int) (Math.random() * 10) );
+	    beck.add( call );
+	}
+	System.out.println( ALHeapMin );
+	System.out.println( ALHeapMax );
+	System.out.println( beck.getMedian() );
 
+	for (int i = 0; i < 5; i++) { // 8 total elements
+	    Integer bop = new Integer( (int) (Math.random() * 10) );
+	    beck.add( bop );
+	}
+	System.out.println( ALHeapMin );
+	System.out.println( ALHeapMax );
+	System.out.println( beck.getMedian() );
+
+	for (int i = 0; i < 5; i++) { // 15 total elements
+	    Integer sha = new Integer( Math.pow( ((int) (Math.random() * 10)), 2 ) ); // square a random number
+	    beck.add( sha );
+	}
+	System.out.println( ALHeapMin );
+	System.out.println( ALHeapMax );
+	System.out.println( beck.getMedian() );	
 	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     }//end main()
